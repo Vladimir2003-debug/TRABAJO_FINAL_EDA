c30552050c2b581015738403a1210a25f5fc03d1
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-10 10:05:53 -0500

    estructuras de datos

---
+++ datastructures/Node.java
@@ -0,0 +1,35 @@
+package datastructures;
+public class Node <T>{
+    private T data;
+    private Node<T> next;
+    
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
+    public Node(T data, Node<T> next){
+        this.data = data;
+        this.next = next;
+    }	
+    
+    public T getData(){
+        return this.data;
+    }
+    
+    public void setData(T data){
+        this.data = data;
+    }
+    
+    public Node<T> getNext(){
+        return this.next;
+    }
+    
+    public void setNext(Node<T> next){
+        this.next = next;
+    }    
+    
+    public String toString(){
+        return this.data.toString();
+    }
+
+}
