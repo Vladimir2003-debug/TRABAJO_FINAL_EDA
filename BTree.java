782e9cbf951b4cc44198d476e9a3e297566dc354
Cvenero <93340347+Cvenero@users.noreply.github.com>
2022-08-15 09:37:07 -0500

    Create BTree.java

---
+++ BTree.java
@@ -0,0 +1,315 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class BTree {
+    public static void main(String[] argumentos) {
+    	
+    	leerTexto("C:\\Users\\acer\\Desktop\\textoPrueba.txt");
+    	/*Scanner scan = new Scanner(System.in);
+    	int numArchivos = scan.nextInt(); 
+    	boolean[] bool = new boolean[3];
+    	*/
+    	
+       /* Arbol arbol = new Arbol();
+        arbol.insertar(1);
+        arbol.insertar(2);
+        arbol.insertar(3);
+        arbol.insertar(4);
+        arbol.insertar(0);
+        System.out.println("Recorriendo inorden:");
+        arbol.inorden();
+        System.out.println("Recorriendo postorden:");
+        arbol.postorden();
+        System.out.println("Recorriendo preorden:");
+        arbol.preorden();
+        System.out.println(arbol.existe(1)); // true
+        System.out.println(arbol.existe(7)); // false
+        System.out.println(arbol.existe(0)); // true*/
+        ArbolCadenas arbolCadenas = new ArbolCadenas();
+        
+        arbolCadenas.insertar("Luis es el primero");
+        arbolCadenas.insertar("Chris es el segundo");
+        arbolCadenas.insertar("Zelda es el tercero");
+        arbolCadenas.insertar("Cuphead es el cuarto");
+        arbolCadenas.insertar("Leon es el quinto");
+        System.out.println("- Recorriendo inorden:");
+        arbolCadenas.inorden();
+        System.out.println("- Recorriendo postorden:");
+        arbolCadenas.postorden();
+        System.out.println("- Recorriendo preorden:");
+        arbolCadenas.preorden();
+        System.out.println(arbolCadenas.existe("Luis")); // true
+        System.out.println(arbolCadenas.existe("Claire")); // false
+        System.out.println(arbolCadenas.existe("Zelda")); // true
+    	
+	}
+	public static String leerTexto(String direccion) {
+		
+		String texto = "";
+		
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader(direccion));
+			String temp = "";
+			String bfRead;
+			while((bfRead = bf.readLine()) != null)  {
+				String[] cadena = bfRead.split(" ");
+				System.out.println(cadena[1]);
+				//temp = temp + bfRead;
+				
+			}
+			
+			texto = temp;
+			
+			
+			
+		}catch (Exception e) {System.out.println("No se puede abrir el archivo");}
+		
+		return texto+ "+";
+		
+		
+	}
+    
+}
+
+class NodoCadena {// NODO
+    private String dato;
+    private NodoCadena izquierda, derecha;
+
+    public NodoCadena(String dato) {
+        this.dato = dato;
+        this.izquierda = this.derecha = null;
+        
+    }
+
+
+    public String getDato() {
+        return dato;
+    }
+
+    public NodoCadena getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(NodoCadena izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public NodoCadena getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(NodoCadena derecha) {
+        this.derecha = derecha;
+    }
+
+    public void imprimirDato() {
+        System.out.println(this.getDato());
+    }
+}
+
+
+class ArbolCadenas {
+    private NodoCadena raiz;
+
+    public ArbolCadenas() {
+
+    }
+
+    public boolean existe(String busqueda) {
+        return existe(this.raiz, busqueda);
+    }
+
+    private boolean existe(NodoCadena n, String busqueda) {
+        if (n == null) {
+            return false;
+        }
+        if (n.getDato().equals(busqueda)) {
+            return true;
+        } else if (busqueda.compareTo(n.getDato()) < 0) {
+            return existe(n.getIzquierda(), busqueda);
+        } else {
+            return existe(n.getDerecha(), busqueda);
+        }
+    }
+
+    public void insertar(String dato) {
+        if (this.raiz == null) {
+            this.raiz = new NodoCadena(dato);
+        } else {
+            this.insertar(this.raiz, dato);
+        }
+    }
+
+    private void insertar(NodoCadena padre, String dato) {
+        if (dato.compareTo(padre.getDato()) > 0) {
+            if (padre.getDerecha() == null) {
+                padre.setDerecha(new NodoCadena(dato));
+            } else {
+                this.insertar(padre.getDerecha(), dato);
+            }
+        } else {
+            if (padre.getIzquierda() == null) {
+                padre.setIzquierda(new NodoCadena(dato));
+            } else {
+                this.insertar(padre.getIzquierda(), dato);
+            }
+        }
+    }
+
+    private void preorden(NodoCadena n) {
+        if (n != null) {
+            n.imprimirDato();
+            preorden(n.getIzquierda());
+            preorden(n.getDerecha());
+        }
+    }
+
+    private void inorden(NodoCadena n) {
+        if (n != null) {
+            inorden(n.getIzquierda());
+            n.imprimirDato();
+            inorden(n.getDerecha());
+        }
+    }
+
+    private void postorden(NodoCadena n) {
+        if (n != null) {
+            postorden(n.getIzquierda());
+            postorden(n.getDerecha());
+            n.imprimirDato();
+        }
+    }
+
+    public void preorden() {
+        this.preorden(this.raiz);
+    }
+
+    public void inorden() {
+        this.inorden(this.raiz);
+    }
+
+    public void postorden() {
+        this.postorden(this.raiz);
+    }
+}
+
+class Nodo {
+    private int dato;
+    private Nodo izquierda, derecha;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.izquierda = this.derecha = null;
+    }
+
+
+    public int getDato() {
+        return dato;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+
+    public void imprimirDato() {
+        System.out.println(this.getDato());
+    }
+}
+
+class Arbol {
+    private Nodo raiz;
+
+    public Arbol() {
+
+    }
+
+    public boolean existe(int busqueda) {
+        return existe(this.raiz, busqueda);
+    }
+
+    private boolean existe(Nodo n, int busqueda) {
+        if (n == null) {
+            return false;
+        }
+        if (n.getDato() == busqueda) {
+            return true;
+        } else if (busqueda < n.getDato()) {
+            return existe(n.getIzquierda(), busqueda);
+        } else {
+            return existe(n.getDerecha(), busqueda);
+        }
+
+    }
+
+    public void insertar(int dato) {
+        if (this.raiz == null) {
+            this.raiz = new Nodo(dato);
+        } else {
+            this.insertar(this.raiz, dato);
+        }
+    }
+
+    private void insertar(Nodo padre, int dato) {
+        if (dato > padre.getDato()) {
+            if (padre.getDerecha() == null) {
+                padre.setDerecha(new Nodo(dato));
+            } else {
+                this.insertar(padre.getDerecha(), dato);
+            }
+        } else {
+            if (padre.getIzquierda() == null) {
+                padre.setIzquierda(new Nodo(dato));
+            } else {
+                this.insertar(padre.getIzquierda(), dato);
+            }
+        }
+    }
+
+    private void preorden(Nodo n) {
+        if (n != null) {
+            n.imprimirDato();
+            preorden(n.getIzquierda());
+            preorden(n.getDerecha());
+        }
+    }
+
+    private void inorden(Nodo n) {
+        if (n != null) {
+            inorden(n.getIzquierda());
+            n.imprimirDato();
+            inorden(n.getDerecha());
+        }
+    }
+
+    private void postorden(Nodo n) {
+        if (n != null) {
+            postorden(n.getIzquierda());
+            postorden(n.getDerecha());
+            n.imprimirDato();
+        }
+    }
+
+    public void preorden() {
+        this.preorden(this.raiz);
+    }
+
+    public void inorden() {
+        this.inorden(this.raiz);
+    }
+
+    public void postorden() {
+        this.postorden(this.raiz);
+    }
+}
