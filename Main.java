a994c792c3c1ad7f79725d9b4bd39b49bfb37d66
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-15 11:18:39 -0500

    interfaz

--- Main.java
+++ Main.java
@@ -2,74 +2,75 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import javax.swing.*;
+
 public class Main extends JFrame {
-    public String path = ".";
-    public String[] paths;
+    private String path = ".";
+    private String[] paths;
+    private JButton btnSelectDir;
+    private JButton btnChecker;
+
     public static PlagiarismChecker pc;
-    public Main(){
+
+    public Main() {
         setTitle("Plagiarism Checker");
-        setSize(300,400);
+        setSize(300, 400);
         setLayout(new FlowLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         createContents();
         setVisible(true);
 
     }
-    private void createContents(){
-        JButton button = new JButton("Select Directory");
-        
-        Listener listener = new Listener();
-        add(button);
-        button.addActionListener(listener);
+
+    private void createContents() {
+        btnSelectDir = new JButton("Select Directory");
+        btnChecker = new JButton("Checker");
+
+        add(btnChecker);
+        add(btnSelectDir);
+
+        btnSelectDir.addActionListener(new Listener());
+        btnChecker.addActionListener(new Listener());
     }
 
     private class Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            JFileChooser chooser;
-            chooser = new JFileChooser();
-            chooser.setCurrentDirectory(new java.io.File("."));
-            chooser.setDialogTitle("Select Directory");
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            if (e.getActionCommand().equals("Select Directory")) {
+                JFileChooser chooser;
+                chooser = new JFileChooser();
+                chooser.setCurrentDirectory(new java.io.File("."));
+                chooser.setDialogTitle("Select Directory");
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-            chooser.setAcceptAllFileFilterUsed(false);
+                chooser.setAcceptAllFileFilterUsed(false);
 
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                path = chooser.getSelectedFile().getAbsolutePath();
-            } else {
-                System.out.println("No Selection ");
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    path = chooser.getSelectedFile().getAbsolutePath();
+                    pc = new PlagiarismChecker();
+
+                    //
+                    File folder = new File("./reuters21578");
+
+                    File[] listOfFiles = folder.listFiles();
+                    String[] paths = new String[listOfFiles.length];
+                    int i = 0;
+                    for (File file : listOfFiles) {
+                        if (file.isFile()) {
+                            paths[i] = file.getPath();
+                        }
+                        i++;
+                    }
+
+                    pc.loadFiles(paths);
+                    pc.getTextFiles()[1].inOrder();
+                    
+                }
+            } else if (e.getActionCommand().equals("Checker")) {
+                System.out.println("fiorella");
             }
         }
     }
 
     public static void main(String[] args) {
         new Main();
-
-        pc = new PlagiarismChecker();
-        File folder = new File("./reuters21578");
-
-        File[] listOfFiles = folder.listFiles();
-        String[] paths = new String[listOfFiles.length];
-        int i = 0;
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                paths[i] = file.getPath();
-            }
-            i++;
-        }
-
-        pc .loadFiles(paths);
-
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-        System.out.println(pc.getTextFiles().poll().inOrder());
-
     }
 }
