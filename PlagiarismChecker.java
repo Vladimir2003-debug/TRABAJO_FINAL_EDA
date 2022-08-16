ab0fab153cb68ac70ca9e918fce0b29b97e8213a
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-15 19:04:59 -0500

    se añaden parrafos

--- PlagiarismChecker.java
+++ PlagiarismChecker.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PlagiarismChecker {
-    public BST<String>[] textFiles; 
+    public BST<String>[] textFiles;
     public BST<String> textToChecker;
 
     /*
@@ -18,16 +18,17 @@ public class PlagiarismChecker {
         // Llenar las estructuras (recomendado)
         // Lectura del archivo (recomendado)
         textFiles = new BST[paths.length];
-        try{
+        try {
             Scanner fileIn;
-            for(int i = 0; i<paths.length; i++) {
-                if(paths[i] == null) continue;
+            for (int i = 0; i < paths.length; i++) {
+                if (paths[i] == null)
+                    continue;
                 fileIn = new Scanner(new FileReader(paths[i]));
-                
+
                 textFiles[i] = inputTreeText(fileIn);
                 fileIn.close();
             }
-        } catch(FileNotFoundException e){ 
+        } catch (FileNotFoundException e) {
             System.out.println("No se pudo cargar los archivos " + e);
             return false;
         }
@@ -45,27 +46,29 @@ public class PlagiarismChecker {
         try {
             Scanner fileIn;
             fileIn = new Scanner(new FileReader(path));
-            textToChecker= inputTreeText(fileIn);
+            textToChecker = inputTreeText(fileIn);
             fileIn.close();
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Archivo no encontrado");
         }
-        
-        ResultChecker result = new ResultChecker(textFiles.length,textFiles,textToChecker);
+
+        ResultChecker result = new ResultChecker(textFiles.length, textFiles, textToChecker);
         // Retornar resultados del sistema (obligatorio)
         return result;
     }
+
     public BST<String> inputTreeText(Scanner fileIn) {
         BST<String> text = new BST<String>();
         String line;
-        
-        while(fileIn.hasNextLine()){
+
+        while (fileIn.hasNextLine()) {
             line = fileIn.nextLine();
-            Pattern pattern = Pattern.compile("<.*></.*>");
-            Matcher matcher = pattern.matcher(line);
-            boolean matchFound = matcher.find();
-            if(!matchFound)  
-                text.insert(line);
+            if (line.indexOf("<BODY>") != -1) {
+                while (line.indexOf("</BODY>") == -1 && fileIn.hasNextLine()) {
+                    line += fileIn.nextLine();
+                }
+                text.insert(line + " : ");
+            }
         }
         return text;
     }
