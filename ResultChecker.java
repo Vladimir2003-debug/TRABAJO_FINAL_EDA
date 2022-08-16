708ecfa402577589e0655b4fb29482d8ec6baf0f
Cvenero <93340347+Cvenero@users.noreply.github.com>
2022-08-15 23:45:00 -0500

    Update ResultChecker.java

--- ResultChecker.java
+++ ResultChecker.java
@@ -20,18 +20,17 @@ private boolean[] result;
      	perc += algoritmoA(x,y);
         perc += algoritmoB(x,y);
         
-        //....... masAlgorimtosv:
+  
         if( perc > 0.5)
               result[z] = true;
-              //aca tengo duda desde que % va el true en las bolas v: de 50% supongoS
 
     }
 	public int algoritmoA(BTree x, BTree y) {
-		//.......compara X cosas
+		
 		return 0; //% de coincidencia
 	}
     public int algoritmoB(BTree x, BTree y) {
-    	//......compara Y cosas
+  
     	return 0; //%coincidencia...
     }
    
